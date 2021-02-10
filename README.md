c49c2fe4dbb582e0bcb48185c70d10c61ccdca7d
David Fontenot <font21@gmail.com>
2021-02-09 22:10:52 -0600

    Begun work on adding Getopt module and sysargv.

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 This program is a factory for any game that needs a few clicks every few minutes within an area.
 It also accepts an area to randomly click in (called a hit box) so that it appears to be human.
 
-# Next Up:
+# Future features, Next Up:
 Build in a flat database that accepts JSON data from a file to define the hitbox.
 http://jsoneditoronline.org/
+
+Accept command line arguments using sysArgv for other options like how many times to run and which JSON file to use.
--- elvenarCollect.py
+++ elvenarCollect.py
@@ -5,25 +5,36 @@
 # It also accepts an area to randomly click in (called a hit box) so that it appears to be human.
 # Next: build in a flat file that accepts JSON data to define the hitbox.
 
+# Program Outline
+# I.	Imported libraries
+# II.	Functions
+# III.	Set main variables
+# IV.	Main program
+# V.	Close Out
 
-#### importing libraries 
+
+
+
+
+#### imported libraries
 import os 
 import time
 import random
+import sys
+import sys, getopt
 import subprocess
 from datetime import datetime
 
-# Wait a bit of time to switch over to the proper window and tab.
-time.sleep(6)
-
 #### DEFINE FUNCTIONs
-def elvToolHitBox(elvShop):
+
+# Define Hit Box and mouse movement.
+# Accepts two Paramaters: Which Workshop and hover|click
+def elvToolHitBox(elvShop, hc):
 	randWait = random.randrange(2,9)
 	if elvShop == 1:
 		#elvShop1
 		elvToolHitBox0x = random.randrange(219,269)
 		elvToolHitBox0y = random.randrange(820,869)
-
 	if elvShop == 2:
 		#elvShop2
 		elvToolHitBox0x = random.randrange(320,375)
@@ -44,49 +55,19 @@ def elvToolHitBox(elvShop):
 		#elvFiveMins
 		elvToolHitBox0x = random.randrange(566,694)
 		elvToolHitBox0y = random.randrange(393,512)
+	
+	# Mouse: Hover or click
+	if hc == 0:
+		mouseRunCommand = 'xdotool mousemove ' + str(elvToolHitBox0x) + ' ' + str(elvToolHitBox0y)'
+	if hc == 1:
+		mouseRunCommand = 'xdotool mousemove ' + str(elvToolHitBox0x) + ' ' + str(elvToolHitBox0y) + ' click 1 &'
 
-	# click mouse
-	mouseRunCommand = 'xdotool mousemove ' + str(elvToolHitBox0x) + ' ' + str(elvToolHitBox0y) + ' click 1 &'
 	data = subprocess.Popen([mouseRunCommand], shell=True)
 	output = data.communicate()
 	time.sleep(randWait)
 
-# Hover
-def elvHoverHitBox(elvShop):
-	randWait = random.randrange(2,9)
-	if elvShop == 1:
-		#elvShop1
-		elvToolHitBox0x = random.randrange(219,269)
-		elvToolHitBox0y = random.randrange(820,869)
 
-	if elvShop == 2:
-		#elvShop2
-		elvToolHitBox0x = random.randrange(320,375)
-		elvToolHitBox0y = random.randrange(797,840)
-	if elvShop == 3:
-		#elvShop3
-		elvToolHitBox0x = random.randrange(433,475)
-		elvToolHitBox0y = random.randrange(732,770)
-	if elvShop == 4:
-		#elvShop4
-		elvToolHitBox0x = random.randrange(513,560)
-		elvToolHitBox0y = random.randrange(686,726)
-	if elvShop == 5:
-		#elvSelectAll
-		elvToolHitBox0x = random.randrange(844,860)
-		elvToolHitBox0y = random.randrange(329,345)
-	if elvShop == 6:
-		#elvFiveMins
-		elvToolHitBox0x = random.randrange(566,694)
-		elvToolHitBox0y = random.randrange(393,512)
-
-	# click mouse
-	mouseRunCommand = 'xdotool mousemove ' + str(elvToolHitBox0x) + ' ' + str(elvToolHitBox0y) + ' click 1 &'
-	data = subprocess.Popen([mouseRunCommand], shell=True)
-	output = data.communicate()
-	time.sleep(randWait)
-
-#Harvest
+#Harvest Function: Clikck and drag from one point to another in order to harvest tools from Workshops
 def elvHarvestHitBox(elvShop):
 	randWait = random.randrange(2,9)
 	if elvShop == 1:
@@ -95,25 +76,9 @@ def elvHarvestHitBox(elvShop):
 		elvToolHitBox0y = random.randrange(820,869)
 
 	if elvShop == 2:
-		#elvShop2
-		elvToolHitBox0x = random.randrange(320,375)
-		elvToolHitBox0y = random.randrange(797,840)
-	if elvShop == 3:
-		#elvShop3
-		elvToolHitBox0x = random.randrange(433,475)
-		elvToolHitBox0y = random.randrange(732,770)
-	if elvShop == 4:
 		#elvShop4
 		elvToolHitBox0x = random.randrange(513,560)
 		elvToolHitBox0y = random.randrange(686,726)
-	if elvShop == 5:
-		#elvSelectAll
-		elvToolHitBox0x = random.randrange(844,860)
-		elvToolHitBox0y = random.randrange(329,345)
-	if elvShop == 6:
-		#elvFiveMins
-		elvToolHitBox0x = random.randrange(566,694)
-		elvToolHitBox0y = random.randrange(393,512)
 
 	# click mouse
 	mouseRunCommand = 'xdotool mousemove ' + str(elvToolHitBox0x) + ' ' + str(elvToolHitBox0y) + ' click 1 &'
@@ -121,8 +86,41 @@ def elvHarvestHitBox(elvShop):
 	output = data.communicate()
 	time.sleep(randWait)
 
+
+
+def main(argv):
+	helpText = 'elvenarCollect.py -i 6 -l 36'
+	initialSleepTime = ''
+	l = ''
+	try:
+		opts, args = getopt.getopt(argv,"hil",["initialWait=","looper="])
+	except getopt.GetoptError:
+		print helpText
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print helpText
+			sys.exit()
+		elif opt in ("-i", "--initialWait"):
+			initialWait = arg
+		elif opt in ("-o", "--looper"):
+			looper = arg
+	print 'Input file is "', inputfile
+	print 'Output file is "', outputfile
+
+
+#### Set main variables
+l = 24
+initialWait = 6
+
+# Wait a bit of time to switch over to the proper window and tab.
+time.sleep(initialWait)
+
+# Main program
 #### While loop to collect tools for one hour by looping a dozen times every five minutes
-l = 12
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
 while l > 0:
 	#### Time Stamp
 	now = datetime.now()
@@ -137,13 +135,13 @@ while l > 0:
 
 	# Click Select All Button in Pop up
 	print('Producing.')
-	elvToolHitBox(5)
+	elvToolHitBox(5, 1)
 
 	# Click 5 minutes
-	elvToolHitBox(6)
+	elvToolHitBox(6, 1)
 
-	# Hangout for about five to six minutes
-	randWait = random.randrange(310,350)
+	# Hangout for about five to six minutes to appear human
+	randWait = random.randrange(303,333)
 	print('Waiting for ', randWait, ' seconds.')
 	time.sleep(randWait)
 
@@ -151,16 +149,17 @@ while l > 0:
 	j = 4
 	while j > 0:
 		randWait = random.randrange(2,4)
-		elvToolHitBox(j)
+		elvToolHitBox(j, 1)
 		j -= 1
 
-	# Move mouse over a workshop
-	# w = random.randrange(2,9)
-	# elvToolHitBox(w)
+	# Move mouse over a workshop and hover without clicking just to get info and timing information
+	w = random.randrange(1,4)
+	elvToolHitBox(w, 0)
 
 	print('Collected Tools. Completed loop ', l, '.')
 	l -= 1
 
+# Close Out
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print('Program Completion time: ', current_time)
